189a645f03c87acbab15807e3dcb465fcf3c86c0
Jacob Hajjar <jacob.hajjar1@gmail.com>
2022-11-07 20:07:02 -0800

    ADD NEW files

---
+++ assignment3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from pydoc import doc
+import sys
+import time
+import math
+import pandas as pd
+import numpy as np
+from numpy import linalg as LA
+from sklearn import linear_model, preprocessing
+from sklearn.metrics import mean_squared_error, r2_score
+
+''' '''
+
+__author__ = 'Jacob Hajjar, Michael-Ken Okolo'
+__email__ = 'hajjarj@csu.fullerton.edu, michaelken.okolo1@csu.fullerton.edu'
+__maintainer__ = 'jacobhajjar, michaelkenokolo'
+
+
+def main():
+    """the main function"""
+    
+
+if __name__ == '__main__':
+    main()
